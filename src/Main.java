5ee8fd85e76b4a9c25c1da6eec9f4d8c1c1c54d4
Zerpa <alejandro09.zerpa@gmail.com>
2022-08-18 09:40:55 -0400

    then.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(suma(12,5,20));
+
+        carro micarro = new carro();
+        micarro.aggpuerta();
+        micarro.aggpuerta();
+    }
+    public static int suma(int num1, int num2,int num3){
+
+        return num1+num2+num3;
+    }
+}
---
+++ src/carro.java
@@ -0,0 +1,11 @@
+//clase carro
+public class carro{
+    private int puertas;
+
+    //metodo para agregar una puerta y mostrar el numero de puertas totales
+    public void aggpuerta(){
+        puertas++;
+        System.out.println("Numero de puertas del carro "+puertas);
+    }
+
+}
